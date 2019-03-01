b81a660e12c8e37440286eac60576c36c1fa1e78
Gobdomino <nateball13@gmail.com>
2019-02-28 23:08:02 -0500

    Adding changes 2

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers  (name, devoured) VALUES ('Bacon', true);
+INSERT INTO burgers  (name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double', true);
